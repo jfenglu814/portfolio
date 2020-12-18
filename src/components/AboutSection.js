276643bcfa99feba41efb795385e43f99340df91
Jeffrey Lu <lufg@mail.uc.edu>
2020-12-18 10:08:18 -0500

    Styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,43 +1,62 @@
-import React from 'react';
-import home1 from '../img/home1.png';
-import styled from 'styled-components';
+import React from "react";
+import home1 from "../img/home1.png";
+import About from "../Images/About.jpg";
+import styled from "styled-components";
 
 const AboutSection = () => {
-    return (  
-        <StyledAbout>
-            <div className="description">
-                <div className="title">
-                    <div className="hide">
-                        <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>
-                            your <span>dreams</span> come
-                        </h2>
-                    </div>
-                    <div className="hide">
-                        <h2>true.</h2>
-                    </div>
-                </div>
-                <p>Contact me for any questions.</p>
-            </div>
-            <div className="image">
-                <div className="image">
-                    <img src={home1} alt="guy with camera"/>
-                </div>
-            </div>
-        </StyledAbout>
-    );
-}
- 
+  return (
+    <StyledAbout>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>Working to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
+        </div>
+        <p>Contact me for any questions.</p>
+        <button>Contact</button>
+      </Description>
+      <Image>
+        <img src={About} alt="guy with camera" />
+      </Image>
+    </StyledAbout>
+  );
+};
+
 //styled components
 const StyledAbout = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 *{
@@ -15,7 +15,7 @@ button{
     font-weight: bold;
     font-size: 1.1.rem;
     cursor:pointer;
-    padding:1rem 2 rem;
+    padding:1rem 2rem;
     border: 3px solid #23d997;
     background: transparent;
     color:white;
@@ -25,6 +25,30 @@ button{
         color: white;
     }
 }
+h2{
+        font-weight:lighter;
+        font-size: 4rem;
+    }
+    h3{
+        color:white;
+    }
+    h4{
+        font-weight: bold;
+        
+    }
+    a{
+        font-size: 1.1rem;
+    }
+    span{
+        font-weight:bold;
+        color: #23d997;
+    }
+    p{
+        padding: 3rem 0rem;
+        color: #ccc;
+        font-size: 1.4rem;
+        line-height: 150%;
+    }
 
 `;
 
