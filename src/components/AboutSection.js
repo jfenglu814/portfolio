edb611bbb36eb56eb6259eccd62c3ccd65bc8455
Jeffrey Lu <lufg@mail.uc.edu>
2020-12-18 11:04:41 -0500

    Styled ProjectsSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,11 +1,14 @@
 import React from "react";
+
+//import images
 import home1 from "../img/home1.png";
-import About from "../Images/About.jpg";
-import styled from "styled-components";
+import AboutImage from "../Images/About.jpg";
+
+import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
-    <StyledAbout>
+    <About>
       <Description>
         <div className="title">
           <Hide>
@@ -24,39 +27,12 @@ const AboutSection = () => {
         <button>Contact</button>
       </Description>
       <Image>
-        <img src={About} alt="guy with camera" />
+        <img src={AboutImage} alt="guy with camera" />
       </Image>
-    </StyledAbout>
+    </About>
   );
 };
 
 //styled components
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,52 +1,84 @@
-import React from 'react';
+import React from "react";
 //Import Icons
-import clock from '../img/clock.svg';
-import diaphram from '../img/diaphragm.svg';
-import money from '../img/money.svg';
-import teamwork from '../img/teamwork.svg';
-import home2 from '../img/home2.png'
+import clock from "../img/clock.svg";
+import diaphram from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+import home2 from "../img/home2.png";
+
+//Styles
+import { About, Description, Image } from "../styles";
+import styled from "styled-components";
 
 const ProjectsSection = () => {
-    return (
-        <div className="services">
-          <div className="description">
-            <h2>High <span>quality</span> services</h2>
-            <div className="cards">
-              <div className="card">
-                <div className="icon">
-                  <img src={clock} alt="project1"/>
-                  <h3>Efficient</h3>
-                </div>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div className="card">
-                <div className="icon">
-                  <img src={teamwork} alt="project2"/>
-                  <h3>Teamwork</h3>
-                </div>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div className="card">
-                <div className="icon">
-                  <img src={diaphram} alt="project3"/>
-                  <h3>Diaphram</h3>
-                </div>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div className="card">
-                <div className="icon">
-                  <img src={money} alt="project4"/>
-                  <h3>Affordable</h3>
-                </div>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </div>
+  return (
+    <Projects>
+      <Description>
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <Cards>
+          <Card>
+            <div className="icon">
+              <img src={clock} alt="project1" />
+              <h3>Efficient</h3>
             </div>
-          </div>
-          <div className="image">
-            <img src={home2} alt="picture"/>
-          </div>
-        </div>
-      );
-}
- 
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={teamwork} alt="project2" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={diaphram} alt="project3" />
+              <h3>Diaphram</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={money} alt="project4" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
+        <img src={home2} alt="picture" />
+      </Image>
+    </Projects>
+  );
+};
+
+const Projects = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 export default ProjectsSection;
