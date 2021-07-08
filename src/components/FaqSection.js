c3d9c8f06ac398a2d909f65de9fcc654ab0cd90b
Jeffrey Lu <lufg@mail.uc.edu>
2021-07-07 21:50:23 -0400

    updated work experience

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -20,7 +20,7 @@ const FaqSection = () => {
       <div className="question">
         <h4>Education</h4>
         <div className="answer">
-          <p>University of Cincinnati. Master's of Information Technology.</p>
+          <p>University of Cincinnati. MS, Information Technology</p>
           <p>
             Pennsylvania State University. Bachelor's of Science, Life Science.
           </p>
@@ -30,11 +30,19 @@ const FaqSection = () => {
       <div className="question">
         <h4>Digital Technology Intern, GE Aviation</h4>
         <div className="answer">
-          <p>Worked in a cross-functional and global Agile team.</p>
-          <p>Created strategies to simplify and optimize data flow processes</p>
           <p>
-            Coordinated and developed testing for the implementation of a large
-            SAAS.
+            Headed the QA effort in the development of a FedRAMP ServiceNow
+            instance for a space within GE Aviation.
+          </p>
+          <p>
+            Administered the testing and validation of over 115 user stories.
+          </p>
+          <p>
+            Delivered test plans for 5 development sprints and 1 UAT sprint.
+          </p>
+          <p>
+            Authored training materials for Ops personnel on the implementation
+            of the FedRamp ServiceNow instance.
           </p>
         </div>
         <div className="faq-line"></div>
@@ -42,14 +50,21 @@ const FaqSection = () => {
       <div className="question">
         <h4>Software Development Internship, UC IT Solutions Center</h4>
         <div className="answer">
-          <p>Followed agile methodology of the Software Development Cycle</p>
           <p>
-            Implemented performance optimization on PostgreSQL database to
-            increase by performance up to 90%.
+            Collaborated with a global Agile team doing full-stack development
+            using React, Node, and PostgreSQL.
           </p>
           <p>
-            Resolved technical issues in Risk Assessment Application used by
-            multiple government agencies
+            Developed and implemented feature requests from clients involving UI
+            changes and complex data integration.
+          </p>
+          <p>
+            Designed index and optimized queries for database performance tuning
+            yielding up to a 90% increase in performance.
+          </p>
+          <p>
+            Supported the migration of the flagship Risk Assessment application
+            from Angular to React..
           </p>
         </div>
         <div className="faq-line"></div>
